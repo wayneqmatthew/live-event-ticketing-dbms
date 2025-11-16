debbeda48bda7d9954614a01d2d8aea7f499903d
wayneqmatthew <wayneqmatthew@gmail.com>
2025-11-17 00:12:51 +0800

    added sql files

---
+++ src/main/sql/mockdata.sql
@@ -0,0 +1,135 @@
+-- =============================================
+-- INSERTING MOCK DATA INTO TICKETING DATABASE
+-- CCINFOM S21
+-- Group 2: Bueno, Jonathan R.
+-- 			De Gracia, Kaleela Ysabel B.
+-- 			Guanzon, Melangelo P.
+-- 			Quebrado, Wayne Matthew V.
+-- =============================================
+
+-- =============================
+-- Dumping data for table Venue
+-- =============================
+LOCK TABLES Venue WRITE;
+/*!40000 ALTER TABLE Venue DISABLE KEYS */;
+INSERT INTO Venue (name, capacity, city, country, region, status) VALUES
+('Mall of Asia Arena', 15000, 'Pasay City', 'Philippines', 'Asia', 'Active'),
+('Philippine Arena', 55000, 'Bocaue', 'Philippines', 'Asia', 'Active'),
+('Smart Araneta Coliseum', 16000, 'Quezon City', 'Philippines', 'Asia', 'Active'),
+('Seoul Olympic Stadium', 68900, 'Songpa-gu District', 'South Korea', 'Asia', 'Active'),
+('Jakarta International Stadium', 82000, 'Jakarta', 'Indonesia', 'Asia', 'Active'),
+('SM North Edsa Skydome', 1500, 'Quezon City', 'Philippines', 'Asia', 'Active'),
+('Beijing National Stadium', 80000, 'Chaoyang', 'China', 'Asia', 'Active'),
+('Husky Stadium', 70038, 'Seattle', 'United States', 'North America', 'Active'),
+('Nissan Stadium', 72327, 'Yokohama', 'Japan', 'Asia', 'Active'),
+('SoFi Stadium', 70240, 'Inglewood', 'United States', 'North America', 'Active');
+/*!40000 ALTER TABLE Venue ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- ==============================
+-- Dumping data for table Artist
+-- ==============================
+LOCK TABLES Artist WRITE;
+/*!40000 ALTER TABLE Artist DISABLE KEYS */;
+INSERT INTO Artist (name, email, genre, management_company, status) VALUES
+('Taylor Swift', 'taylorswift@gmail.com', 'Pop', '13 Management', 'Active'),
+('Sabrina Carpenter', 'sabrinacarpenter@gmail.com', 'Pop', 'Volara Management', 'Active'),
+('SEVENTEEN', 'seventeen@gmail.com', 'Kpop', 'HYBE Entertainment', 'Active'),
+('Lea Salonga', 'leasalonga@gmail.com', 'OPM', NULL, 'Active'),
+('Jose Mari Chan', 'josemarichan@gmail.com', 'Pop', NULL, 'Active'),
+('EJ Miranda', 'ejmiranda@gmail.com', 'Christian Music', NULL, 'Active'),
+('BTS', 'bts@gmail.com', 'Kpop', 'HYBE Entertainment', 'Active'),
+('TWICE', 'twice@gmail.com', 'Kpop', 'JYP Entertainment', 'Active'),
+('KATSEYE', 'katseye@gmail.com', 'Pop', 'HYBE Entertainment', 'Active'),
+('ARTMS', 'artms@gmail.com', 'Kpop', 'Modhaus Incorporated', 'Active');
+/*!40000 ALTER TABLE Artist ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- =================================
+-- Dumping data for table Organizer
+-- =================================
+LOCK TABLES Organizer WRITE;
+/*!40000 ALTER TABLE Organizer DISABLE KEYS */;
+INSERT INTO Organizer (name, email, city, country, region, status) VALUES
+('Live Nation PH', 'livenation@gmail.com', 'Taguig City', 'Philippines', 'Asia', 'Active'),
+('PULP Live World', 'pulpliveworld@gmail.com', 'Quezon City', 'Philippines', 'Asia', 'Active'),
+('Insignia Presents', 'insigniapresents@gmail.com', 'Taguig City', 'Philippines', 'Asia', 'Active'),
+('SHOWNOTE Incorporated', 'shownote@gmail.com', 'Seoul', 'South Korea', 'Asia', 'Active'),
+('FNC Entertainment', 'fncent@gmail.com', 'Seoul', 'South Korea', 'Asia', 'Active'),
+('TINYgMUSIC', 'tinygmusic@gmail.com', 'London', 'United Kingdom', 'Europe', 'Active'),
+('Anschutz Entertainment Group', 'aegpresents@gmail.com', 'London', 'United Kingdom', 'Europe', 'Active'),
+('Insomniac Events', 'insomniacevents@gmail.com', 'Orlando', 'United Sates', 'North America', 'Active'),
+('Music Viva Australia', 'musicvivaaus@gmail.com', 'New South Wales', 'Australia', 'Australia', 'Active'),
+('Untitled Group', 'untitledgroup@gmail.com', 'Melbourne', 'Australia', 'Australia', 'Active'),
+('Smash', 'smash@gmail.com', 'Osaka', 'Japan', 'Asia', 'Active');
+/*!40000 ALTER TABLE Organizer ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- ================================
+-- Dumping data for table Customer
+-- ================================
+LOCK TABLES Customer WRITE;
+/*!40000 ALTER TABLE Customer DISABLE KEYS */;
+INSERT INTO Customer (last_name, first_name, email, phone_number, registration_date, status) VALUES
+('Harrison', 'Tyson', 'harrisontyson@gmail.com', '09370976099', '2022-02-28', 'Active'),
+('Li', 'Hosea', 'hoseali@gmail.com', '09788507978', '2023-12-15', 'Active'),
+('Woodward', 'Sandford', 'sandfordwood@gmail.com', '09977676722', '2023-08-30', 'Active'),
+('Silva', 'Juliet', 'julietsilva@gmail.com', '09107202889', '2023-08-13', 'Active'),
+('Andrews', 'Abigail', 'abigailandrews@gmail.com', '09491162032', '2021-08-31', 'Active'),
+('Johns', 'Herbert', 'herbertjohns@gmail.com', '09623464605', '2021-08-08', 'Active'),
+('Pacheco', 'Lucile', 'lucilepacheco@gmail.com', '09445388236', '2018-05-06', 'Active'),
+('Mathis', 'Lonny', 'lonnymathis@gmail.com', '09459034062', '2017-12-10', 'Active'),
+('Tate', 'Gale', 'galetate@gmail.com', '09936226463', '2017-09-04', 'Active'),
+('Holder', 'Michael', 'michaelholder@gmail.com', '09491231816', '2017-08-25', 'Active'),
+('De Gracia', 'Kaleela', 'kaleelaysabel@gmail.com', '09957451018', '2025-11-13', 'Active');
+/*!40000 ALTER TABLE Customer ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- =============================
+-- Dumping data for table Event
+-- =============================
+/*!40000 ALTER TABLE Event DISABLE KEYS */;
+INSERT INTO Event (venue_id, artist_id, organizer_id, event_name, time, date, capacity, status) VALUES
+(7, 1, 3, 'The Eras Tour', '18:00:00', '2026-03-09', '70000', 'Upcoming'),
+(6, 2, 1, 'Short n Sweet Tour', '18:00:00', '2025-03-17', '1500', 'Done'),
+(5, 4, 6, 'Stage, Screen and Everything In Between', '19:00:00', '2026-06-18', 81500, 'Upcoming'),
+(3, 5, 4, 'Here and Now: Celebrating the Music of Jose Mari Chan', '17:00:00', '2025-11-25', 16000, 'Upcoming'),
+(3, 7, 4, 'Permission to Dance on Stage', '18:00:00', '2027-02-28', 16000, 'Upcoming'),
+(6, 1, 6, 'The Eras Tour', '19:00:00', '2025-10-28', 1500, 'Done'),
+(9, 10, 6, 'Lunar Theory World Tour', '20:00:00', '2025-05-14', 72300, 'Done'),
+(4, 8, 1, 'This is For: World Tour', '19:00:00', '2025-10-04', 68900, 'Done'),
+(1, 2, 7, 'Short n Sweet Tour', '18:00:00', '2025-07-18', '14500', 'Done');
+/*!40000 ALTER TABLE Event ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- ==============================
+-- Dumping data for table Ticket
+-- ==============================
+/*!40000 ALTER TABLE Ticket DISABLE KEYS */;
+INSERT INTO Ticket (event_id, customer_id, seat_number, purchase_date, price, status) VALUES
+(4, 5, 'J39', '2025-03-28', 4700.00, 'Cancelled'),
+(3, 2, 'B20', '2026-02-28', 15699.00, 'Active'),
+(1, 9, 'H27', '2026-12-03', 5700.00, 'Active'),
+(5, 10, 'A13', '2025-12-03', 7400.00, 'Cancelled'),
+(5, 8, 'E30', '2026-09-22', 15100.00, 'Active');
+/*!40000 ALTER TABLE Ticket ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- ====================================
+-- Dumping data for table Cancellation
+-- ====================================
+/*!40000 ALTER TABLE Cancellation DISABLE KEYS */;
+INSERT INTO Cancellation (ticket_id, refund_amount, cancellation_date) VALUES
+(1, 4700.00, '2025-04-10'),
+(4, 7400.00, '2025-12-25');
+/*!40000 ALTER TABLE Cancellation ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- ========================================
+-- Dumping data for table CommissionPayout
+-- ========================================
+/*!40000 ALTER TABLE CommissionPayout DISABLE KEYS */;
+INSERT INTO CommissionPayout (artist_id, organizer_id, commission_amount, total_ticket_sales, commission_percentage, payout_date) VALUES
+(2, NULL, 195000.00, 1500000.00, 13.00, '2025-04-01'),
+(NULL, 6, 1050000.00, 7000000.00, 15.00, '2025-11-22'),
+(NULL, 7, 468000.00, 39000000.00, 12.00, '2025-08-12');
---
+++ src/main/sql/ticketsystem.sql
@@ -0,0 +1,176 @@
+-- =============================================
+-- TICKETING DATABASE INITIALIZATION SCRIPT
+-- CCINFOM S21
+-- Group 2: Bueno, Jonathan R.
+-- 			De Gracia, Kaleela Ysabel B.
+-- 			Guanzon, Melangelo P.
+-- 			Quebrado, Wayne Matthew V.
+-- =============================================
+
+-- Drop and recreate the user with correct password
+DROP USER IF EXISTS 'root'@'localhost';
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'admin';
+
+-- Grant all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+DROP DATABASE IF EXISTS ticketsystem;
+CREATE DATABASE ticketsystem/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE ticketsystem;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- ============================
+-- Table setup for Venue table
+-- ============================
+DROP TABLE IF EXISTS Venue;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Venue (
+    venue_id INT PRIMARY KEY AUTO_INCREMENT,		-- Unique ID for each venue
+    name VARCHAR(100),				-- Name of the venue
+    capacity INT,					-- Seating capacity
+    city VARCHAR(50),				-- City where the venue is located
+    country VARCHAR(50),			-- Country of the venue
+    region VARCHAR(50),				-- Region
+    status VARCHAR(15)              -- Status
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+-- =============================
+-- Table setup for Artist table
+-- =============================
+DROP TABLE IF EXISTS Artist;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Artist (
+    artist_id INT PRIMARY KEY AUTO_INCREMENT,				-- Unique ID for each artist
+    name VARCHAR (100),                     -- Artist name
+    email VARCHAR (255),                    -- Artist email
+    genre VARCHAR(50),						-- Genre of the artist
+    management_company VARCHAR(100),		-- Name of the managing company
+    status VARCHAR(15)                      -- status
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ================================
+-- Table setup for Organizer table
+-- ================================
+DROP TABLE IF EXISTS Organizer;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Organizer (
+    organizer_id INT PRIMARY KEY AUTO_INCREMENT,		-- Unique ID for each organizer
+    name VARCHAR (100),                 -- name of organizer
+    email VARCHAR (255),                -- email of organizer
+    region VARCHAR(50),					-- Region where it is located
+    country VARCHAR(50),				-- Country where it is located
+    city VARCHAR(50),					-- City where it is located
+    status VARCHAR(15)                  -- status
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ================================
+-- Table setup for Customer venuetable
+-- ================================
+DROP TABLE IF EXISTS Customer;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Customer (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,		-- Unique ID for each customer
+    last_name VARCHAR(50),				-- Customer's last name
+    first_name VARCHAR(50),				-- Customer's first name		
+    email VARCHAR(100),					-- Customer's email
+    phone_number VARCHAR(20),			-- Customer's phone number		
+    registration_date date,				-- Date when customer registered
+    status VARCHAR(15)                  -- status
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ================================
+-- Table setup for Event table
+-- ================================
+DROP TABLE IF EXISTS Event;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Event (
+    event_id INT PRIMARY KEY AUTO_INCREMENT,		-- Unique ID fore each event
+    venue_id INT,					-- Foreign key connecting Event to Venue
+    artist_id INT,					-- Foreign key connecting Event to Artist
+    organizer_id INT,				-- Foreign key connecting Event to Organizer
+    event_name VARCHAR(100),		-- Name of the event
+    time TIME,						-- Time of the event
+    date DATE,						-- Date of the event
+    capacity INT,					-- Number of tickets the event can sell
+    status VARCHAR(20),				-- The status of the ticket (i.e "Upcoming")
+    category VARCHAR(50),			-- Category of the event
+    FOREIGN KEY (venue_id) REFERENCES Venue(venue_id),
+    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
+    FOREIGN KEY (organizer_id) REFERENCES Organizer(organizer_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ===================================
+-- Table setup for Cancellation table
+-- ===================================
+DROP TABLE IF EXISTS Cancellation;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Cancellation (
+    cancellation_ref_id INT PRIMARY KEY AUTO_INCREMENT,		-- Unique ID for each cancellation
+    ticket_id INT,								-- Foreign key connecting Cancellation to Ticket
+    refund_amount DECIMAL(10,2),				-- Amount to refund
+    cancellation_date DATE,						-- Date the ticket was cancelled
+    FOREIGN KEY (ticket_id) REFERENCES Ticket(ticket_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ================================
+-- Table setup for Ticket table
+-- ================================
+DROP TABLE IF EXISTS Ticket;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE Ticket (
+    ticket_id INT PRIMARY KEY AUTO_INCREMENT,			-- Unique ID for each ticket
+    event_id INT,						-- Foreign key connecting Ticket to Event
+    customer_id INT,					-- Foreign key connecting Ticket to Customer
+    seat_number VARCHAR(10),			-- Seat number on the ticket
+    purchase_date DATE,					-- Date when the ticket was purchased
+    price DECIMAL(10,2),				-- Price of the ticket
+    status VARCHAR(20),					-- Status of the ticket (i.e "Active", "Cancelled")
+    FOREIGN KEY (event_id) REFERENCES Event(event_id),
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- =======================================
+-- Table setup for CommissionPayout table
+-- =======================================
+DROP TABLE IF EXISTS CommissionPayout;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE CommissionPayout (
+    payout_ref_id INT PRIMARY KEY AUTO_INCREMENT,			-- Unique ID for each payout
+    artist_id INT,							-- artist_id for artist payout
+    organizer_id INT,                       -- organizer_id for organizer payout
+    commission_amount DECIMAL(10,2),		-- Total commission amount
+    total_ticket_sales INT,					-- Total sales from tickets
+    commission_percentage DECIMAL(5,2),		-- Commission rate of the organizer/artist
+    payout_date DATE,						-- Date when the amount was paid
+    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
+    FOREIGN KEY (organizer_id) REFERENCES Organizer(organizer_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
